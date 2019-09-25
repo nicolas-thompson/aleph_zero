e4c00b07c200b7a322f06d1a8df2e0449fff092c
Nicolas Thompson <mail@nicolasthompson.com>
2019-09-25 14:27:24 +0100

    initial commit

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+    
+    /** @test */
+    public function the_database_is_populated_with_users() 
+    {
+        $this->assertDatabaseHas('User', [
+            'first_name' => 'Simon',
+        ]);
+    }
+}
