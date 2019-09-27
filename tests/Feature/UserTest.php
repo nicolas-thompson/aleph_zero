238d3dc406704f0279940ab8623577581382ee26
Nicolas Thompson <mail@nicolasthompson.com>
2019-09-27 10:14:13 +0100

    updates test with a reasonable data set

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -13,14 +13,34 @@ class UserTest extends TestCase
     /** @test */
     public function the_database_is_populated_with_users() 
     {
-        factory('App\User', 1200)->create([
-            'first_name' => 'Simon',
-            'last_name' => 'Wright',
+        factory('App\User', 1)->create([
+            'first_name' => 'Adam',
+            'last_name' => 'Ant',
+        ]);
+
+        factory('App\User', 1)->create([
+            'first_name' => 'Michal',
+            'last_name' => 'Manson',
+        ]);
+
+        factory('App\User', 1)->create([
+            'first_name' => 'Ziv',
+            'last_name' => 'Zaifman',
         ]);
 
         $this->assertDatabaseHas('Users', [
-            'first_name' => 'Simon',
-            'last_name' => 'Wright',
+            'first_name' => 'Adam',
+            'last_name' => 'Ant',
+        ]);
+
+        $this->assertDatabaseHas('Users', [
+            'first_name' => 'Michal',
+            'last_name' => 'Manson',
+        ]);
+
+        $this->assertDatabaseHas('Users', [
+            'first_name' => 'Ziv',
+            'last_name' => 'Zaifman',
         ]);
     }
 }
