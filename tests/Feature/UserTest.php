6e0f4aeff7bff0813324d8b9420be998571fd424
Nicolas Thompson <mail@nicolasthompson.com>
2019-09-27 10:55:51 +0100

    test it can return a sorted list of matches

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -13,6 +13,8 @@ class UserTest extends TestCase
     /** @test */
     public function the_database_is_populated_with_users() 
     {
+        // create users
+
         factory('App\User', 1)->create([
             'first_name' => 'Adam',
             'last_name' => 'Ant',
@@ -24,9 +26,16 @@ class UserTest extends TestCase
         ]);
 
         factory('App\User', 1)->create([
+            'first_name' => 'Simon',
+            'last_name' => 'Wright',
+        ]);
+
+        factory('App\User', 1)->create([
             'first_name' => 'Ziv',
             'last_name' => 'Zaifman',
         ]);
+
+        // assertions
 
         $this->assertDatabaseHas('Users', [
             'first_name' => 'Adam',
@@ -41,6 +50,48 @@ class UserTest extends TestCase
         $this->assertDatabaseHas('Users', [
             'first_name' => 'Ziv',
             'last_name' => 'Zaifman',
+        ]);
+    }
+
+    /** @test */
+    public function it_can_return_a_sorted_list_of_matches()
+    {
+        factory('App\User', 1)->create([
+            'first_name' => 'Adam',
+            'last_name' => 'Ant',
+        ]);
+
+        factory('App\User', 1)->create([
+            'first_name' => 'Michal',
+            'last_name' => 'Manson',
+        ]);
+
+        factory('App\User', 1)->create([
+            'first_name' => 'Richard',
+            'last_name' => 'Wright',
+        ]);
+
+        factory('App\User', 1)->create([
+            'first_name' => 'Simon',
+            'last_name' => 'Wright',
+        ]);
+
+        factory('App\User', 1)->create([
+            'first_name' => 'Ziv',
+            'last_name' => 'Zaifman',
+        ]);
+
+        $response = $this->json('POST', 'users/search', ['terms' => 'Wright']);
+
+        $response->assertJson([
+            [
+                'first_name' => 'Richard',
+                'last_name' => 'Wright',
+            ],
+            [
+                'first_name' => 'Simon',
+                'last_name' => 'Wright',
+            ],
         ]);
     }
 }
