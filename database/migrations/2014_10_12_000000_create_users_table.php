05799cd8e8884b7be79df51ce21da0c7b318795c
Nicolas Thompson <mail@nicolasthompson.com>
2019-09-27 09:12:00 +0100

    revisiting tests

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('first_name');
             $table->string('last_name');
+            $table->timestamps();
         });
     }
 
